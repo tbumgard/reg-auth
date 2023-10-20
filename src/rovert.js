f6c71046fe88fe0470c9a9f1d6e2ffa2a3527cc2
Workey <danielsal4@gmail.com>
2023-10-20 12:57:39 -0400

    fixed bad text

--- src/rovert.js
+++ src/rovert.js
@@ -2,7 +2,7 @@ function MyButton() {
     return (
         <div>
             <button>
-                CLICK ME IF YOU THINK WORKEY IS A RETARD!!!!
+                CLICK ME IF YOU THINK WORKEY IS A MORON!!!!
             </button>
         </div>
     );

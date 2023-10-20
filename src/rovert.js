e6b0d01b57e5c93167f8aad046c61890e5611975
tbumgard <tbumgard@gmail.com>
2023-10-20 13:45:46 -0400

    Added input fields for username and password.  Although I feel like there might be another way of doing it using a single react component that can take inputs for type and id

--- src/rovert.js
+++ src/rovert.js
@@ -8,10 +8,30 @@ function MyButton() {
     );
 }
 
+function NameInputField(){
+    return (
+        <div>
+            <label>Username:</label>
+            <input type="text" id="username"/>
+        </div>
+    )
+}
+
+function PasswordInputField(){
+    return (
+        <div>
+            <label>Password:</label>
+            <input type="password" id="password"/>
+        </div>
+    )
+}
+
 function Rovert() {
     return (
         <div>
             <h1>Welcome to Rovert's App!</h1>
+            <NameInputField/>
+            <PasswordInputField/>
             <MyButton/>
         </div>
     );

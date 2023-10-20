9cc1b8fe7a576fa5cf3ff4cf7be5fec442ccf166
tbumgard <tbumgard@gmail.com>
2023-10-20 11:22:52 -0400

    Messed around and found out.  Created rovert.js and imported it into index.js.  rovert.js just has the tutorial for creating a button that's from Learn React's page

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Rovert from './rovert';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Rovert />
   </React.StrictMode>
 );
 
---
+++ src/rovert.js
@@ -0,0 +1,20 @@
+function MyButton() {
+    return (
+        <div>
+            <button>
+                This is a button CLICK IT!
+            </button>
+        </div>
+    );
+}
+
+function Rovert() {
+    return (
+        <div>
+            <h1>Welcome to Rovert's App!</h1>
+            <MyButton/>
+        </div>
+    );
+}
+
+export default Rovert;
